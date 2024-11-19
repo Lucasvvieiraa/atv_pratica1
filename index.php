e610ab19a4d6fe90730c10e1e440011f533daf5b
Lucassvvieiraa <lucas_vieira9@estudante.sesisenai.org.br>
2024-11-19 08:54:11 -0300

    Tela de cadastro e php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_server["REQUEST_METHOD"] == "POST") {
+
+    $conn = new mysqli ('localhost', 'root', '', 'gerenciamento_chamados');
+
+    if ($conn->connect_error) {
+        die("Falha com a conexão: " . $conn->connect_error);
+    }
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    if (empty($nome) || empty($email) || empty($telefone)) {
+        echo "Todos os campos são obrigatórios!";
+    } else {
+        if ($conn->query($sql) === TRUE) {
+            echo "Cliente cadastrado com sucesso!";
+        } else {
+            echo "Erro: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    $conn->close();
+}
+?> 
+
